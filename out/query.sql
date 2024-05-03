4498f5fdfd6cfbe6004fe86c12cb3b065b4e43d3
NightKnight <Ahmedatwa866@yahoo.com>
2024-05-03 16:34:29 +0300

    Add cooks inserts

--- out/query.sql
+++ out/query.sql
@@ -76,24 +76,6 @@ SELECT
 FROM
     `Orders`;
 
--- Shipment
-
-SELECT
-    s.supplier_name
-FROM
-    branch AS b
-JOIN 
-    shipment AS sh
-ON
-    b.branch_name = sh.branch_name
-JOIN 
-    supplier AS s
-ON
-    s.supplier_name = sh.supplier_name
-WHERE
-    b.branch_name = 'number 1';
-
-
 -- Tool
 
 SELECT *
