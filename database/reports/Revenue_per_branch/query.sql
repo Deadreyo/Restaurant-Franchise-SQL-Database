0b89de4baee5689b9b924b579d3b6cd318ed19b3
Ali Mohsen <ali_m_2001@hotmail.com>
2024-05-04 22:33:31 +0300

    Update query.sql

--- database/reports/Revenue_per_branch/query.sql
+++ database/reports/Revenue_per_branch/query.sql
@@ -1,5 +1,6 @@
 SELECT Branch.branch_name, SUM(amount_with_discount) AS total_per_branch
-FROM transaction
+FROM Transaction
+WHERE Transaction.date_and_time BETWEEN '2024-03-01' AND '2024-03-30'    
 JOIN Orders ON Orders.order_id = Transaction.order_id
 JOIN Branch ON Branch.branch_name = Orders.branch_name
 GROUP BY Branch.branch_name
