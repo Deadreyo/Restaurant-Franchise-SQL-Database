211076bad2aa5d754341ad827ca450023c0a4f9b
Omar Gamal <112751175+ElecSpartan@users.noreply.github.com>
2024-05-04 23:09:17 +0300

    lalalalaa

--- database/reports/history/query.sql
+++ database/reports/history/query.sql
@@ -1,8 +1,6 @@
-SELECT c.customer_phone,c.first_name,c.last_name,h.order_id,mi.name,h.quantity,h.notes,mi.size,mi.price
+SELECT c.customer_phone,c.first_name,c.last_name,o.order_id,o.date_and_time,o.total_amount
 FROM Customer AS c 
 JOIN Orders AS o ON c.customer_phone = o.customer_phone
-JOIN Has AS h ON h.order_id = o.order_id
-JOIN Menu_item AS mi ON mi.menu_item_id = h.menu_item_id
-WHERE c.customer_phone="+20 102 886 3371"
-ORDER BY h.order_id,mi.name
+WHERE c.customer_phone="+20 125 036 1267"
+ORDER BY o.order_id
 
