e2c5d597bb39035899940ff7cb99512e55e92dcc
Rana <89163034+RanaMohamad21@users.noreply.github.com>
2024-05-04 15:27:01 +0300

    Added Foreign key.sql

--- database/schema/entities/Delivery/table.sql
+++ database/schema/entities/Delivery/table.sql
@@ -2,6 +2,7 @@ CREATE TABLE Delivery(
     order_id INT,
     statuss ENUM('Pending', 'On the Way', 'Delivered') NOT NULL,
     delivery_fee FLOAT NOT NULL,
+    employee_ssn VARCHAR(9),
     feedback SMALLINT CHECK
         (feedback >= 0 AND feedback <= 5),
     address VARCHAR(250) NOT NULL,
