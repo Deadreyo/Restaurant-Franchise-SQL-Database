4e404b360387cf36adc9d6507baa24a49e01f183
Ali Mohsen <ali_m_2001@hotmail.com>
2024-05-04 22:14:50 +0300

    Update query.sql

--- database/reports/Revenue_per_branch/query.sql
+++ database/reports/Revenue_per_branch/query.sql
@@ -1,6 +1,6 @@
 SELECT Branch.branch_name, SUM(amount_with_discount) AS total_per_branch
 FROM transaction
-JOIN orders ON orders.order_id = transaction.order_id
-JOIN Branch ON Branch.branch_name = orders.branch_name
+JOIN Orders ON Orders.order_id = Transaction.order_id
+JOIN Branch ON Branch.branch_name = Orders.branch_name
 GROUP BY Branch.branch_name
 ORDER BY total_per_branch DESC;
