417e97c08a71ff0c8e312e2ccf3bb020b6c8c29e
Rana <89163034+RanaMohamad21@users.noreply.github.com>
2024-05-04 15:24:25 +0300

    removed columns addition.sql

--- database/schema/entities/Orders/alter.sql
+++ database/schema/entities/Orders/alter.sql
@@ -1,5 +1,4 @@
 ALTER TABLE
-    Orders ADD COLUMN customer_phone CHAR(12),
-    ADD COLUMN branch_name VARCHAR(20),
+    Orders
     ADD CONSTRAINT  FOREIGN KEY(customer_phone) REFERENCES Customer(customer_phone) ON UPDATE CASCADE ON DELETE CASCADE,
     ADD CONSTRAINT FOREIGN KEY(branch_name) REFERENCES Branch(branch_name) ON UPDATE CASCADE ON DELETE CASCADE;
