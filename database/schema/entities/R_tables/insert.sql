397bb15f99d811f6bc494be8fe7f86e93f2496d4
2001-hagar <1900640@eng.asu.edu.eg>
2024-05-04 09:22:13 -0700

    reports

--- database/schema/entities/R_tables/insert.sql
+++ database/schema/entities/R_tables/insert.sql
@@ -1,22 +1,6 @@
-INSERT INTO R_tables (branch_name, table_no, capacity)
-VALUES
-    ('Main Branch', 1, 4),
-    ('Main Branch', 2, 6),
-    ('Downtown Branch', 1, 2),
-    ('Downtown Branch', 2, 4),
-    ('Downtown Branch', 3, 8),
-    ('City Center Branch', 1, 6),
-    ('City Center Branch', 2, 4),
-    ('Westside Branch', 1, 3),
-    ('Westside Branch', 2, 5),
-    ('Westside Branch', 3, 2),
-    ('Southside Branch', 1, 6),
-    ('Southside Branch', 2, 4),
-    ('Uptown Branch', 1, 2),
-    ('Uptown Branch', 2, 4),
-    ('Uptown Branch', 3, 8),
-    ('Midtown Branch', 1, 6),
-    ('Midtown Branch', 2, 4),
-    ('Northside Branch', 1, 3),
-    ('Northside Branch', 2, 5),
-    ('Northside Branch', 3, 2);
+INSERT INTO R_tables(
+    branch_name,
+    table_no,
+    capacity
+)
+VALUES('Main Branch', 1, 4),('Main Branch', 2, 6),('Downtown Branch', 1, 2),('Downtown Branch', 2, 4),('Downtown Branch', 3, 8),('City Center Branch', 1, 6),('City Center Branch', 2, 4),('Westside Branch', 1, 3),('Westside Branch', 2, 5),('Westside Branch', 3, 2),('Southside Branch', 1, 6),('Southside Branch', 2, 4),('Uptown Branch', 1, 2),('Uptown Branch', 2, 4),('Uptown Branch', 3, 8),('Midtown Branch', 1, 6),('Midtown Branch', 2, 4),('Northside Branch', 1, 3),('Northside Branch', 2, 5),('Northside Branch', 3, 2);
