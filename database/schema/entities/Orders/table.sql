5bb8a6fcecde63c17e22a25a90c39b0dc8a56951
Omar Gamal <112751175+ElecSpartan@users.noreply.github.com>
2024-05-04 22:30:37 +0300

    شيكو

--- database/schema/entities/Orders/table.sql
+++ database/schema/entities/Orders/table.sql
@@ -3,7 +3,7 @@ CREATE TABLE Orders(
     date_and_time DATETIME NOT NULL,
     total_amount FLOAT NOT NULL,
     general_notes TEXT,
-    customer_phone CHAR(14),
+    customer_phone CHAR(16),
     branch_name VARCHAR(20),
     statuss ENUM(
         'Pending',
