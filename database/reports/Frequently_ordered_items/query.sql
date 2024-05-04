866a89cff90c15598a31b7201e430d4036175eb8
Ali Mohsen <ali_m_2001@hotmail.com>
2024-05-04 22:09:52 +0300

    Update query.sql

--- database/reports/Frequently_ordered_items/query.sql
+++ database/reports/Frequently_ordered_items/query.sql
@@ -1,19 +1,19 @@
 SELECT name, mutual_count
 FROM (
-    SELECT has.menu_item_id, COUNT(has.menu_item_id) AS mutual_count
+    SELECT Has.menu_item_id, COUNT(Has.menu_item_id) AS mutual_count
     FROM (
-        SELECT menu_item.menu_item_id, SUM(has.quantity) AS total_quantity
-        FROM menu_item
-        JOIN has ON menu_item.menu_item_id = has.menu_item_id
+        SELECT Menu_item.menu_item_id, SUM(Has.quantity) AS total_quantity
+        FROM Menu_item
+        JOIN Has ON Menu_item.menu_item_id = Has.menu_item_id
         JOIN Orders ON Has.order_id = Orders.order_id
         WHERE Orders.date_and_time BETWEEN '2024-03-01' AND '2024-03-30'    
-        GROUP BY menu_item.menu_item_id
+        GROUP BY Menu_item.menu_item_id
         ORDER BY total_quantity DESC
         LIMIT 1
-    ) AS most_ordered
-    JOIN has AS subset_orders ON subset_orders.menu_item_id = most_ordered.menu_item_id
-    JOIN has ON has.order_id = subset_orders.order_id
-    GROUP BY has.menu_item_id
-) AS mutual
-JOIN menu_item ON menu_item.menu_item_id = mutual.menu_item_id
+    ) AS Most_ordered
+    JOIN Has AS Subset_orders ON Subset_orders.menu_item_id = Most_ordered.menu_item_id
+    JOIN Has ON Has.order_id = Subset_orders.order_id
+    GROUP BY Has.menu_item_id
+) AS Mutual
+JOIN Menu_item ON Menu_item.menu_item_id = Mutual.menu_item_id
 ORDER BY mutual_count DESC;
