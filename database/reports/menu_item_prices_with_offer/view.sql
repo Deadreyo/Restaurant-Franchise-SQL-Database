6320668d213feb98f2663c98bec14292f783b936
AhmedMohamed50 <85414834+AhmedMohamed50@users.noreply.github.com>
2024-05-04 21:15:47 +0300

    Update view.sql

--- database/reports/menu_item_prices_with_offer/view.sql
+++ database/reports/menu_item_prices_with_offer/view.sql
@@ -5,8 +5,8 @@ SELECT
     mi.price AS original_price,
     mi.price - (mi.price * (o.discount_percentage / 100)) AS price_after_discount
 FROM
-    menu_item mi
-JOIN discounts d ON
+    Menu_item mi
+JOIN Discounts d ON
     mi.menu_item_id = d.menu_item_id
-JOIN offer o ON
+JOIN Offer o ON
     d.offer_id = o.offer_id;
