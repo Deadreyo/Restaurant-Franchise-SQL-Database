25f448093a078ac2c8b39a7374f704914f59d086
NightKnight <Ahmedatwa866@yahoo.com>
2024-05-04 22:05:36 +0300

    finish my report

--- database/reports/every_ingredient_bought_in_timerange/query.sql
+++ database/reports/every_ingredient_bought_in_timerange/query.sql
@@ -1,10 +1,10 @@
 
-SELECT ingredient_name, branch_name, supplier_name, date_and_time, quantity, price, quantity * price AS total_cost
+SELECT Ingredient.ingredient_name, Branch.branch_name, Supplier.supplier_name, date_and_time, quantity, price, quantity * price AS total_cost
 FROM Ingredient, Supplied_in, Shipment, Branch, Supplier
 WHERE Ingredient.ingredient_name = Supplied_in.ingredient_name AND
       Supplied_in.shipment_id = Shipment.shipment_id AND
       Shipment.branch_name = Branch.branch_name AND
       Shipment.supplier_name = Supplier.supplier_name AND
       date_and_time BETWEEN '2020-01-01' AND '2025-12-31'
-ORDER BY ingredient_name, branch_name, supplier_name
+ORDER BY Ingredient.ingredient_name, Branch.branch_name, Shipment.supplier_name
 ;
