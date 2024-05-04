78c2b11b5773c8eb24135d4b6ac1eab4bc5daa83
Omar Gamal <112751175+ElecSpartan@users.noreply.github.com>
2024-05-04 17:53:53 +0300

    blabla again

--- database/schema/entities/delivery_guy/alter.sql
+++ database/schema/entities/delivery_guy/alter.sql
@@ -1,4 +1,4 @@
-ALTER TABLE Delivery_feedbacks_received
+ALTER TABLE Delivery_guy
 	ADD CONSTRAINT FOREIGN KEY (employee_ssn) REFERENCES Employee(employee_ssn)
     ON UPDATE CASCADE
     ON DELETE CASCADE
